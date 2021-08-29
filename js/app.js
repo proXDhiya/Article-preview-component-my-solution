12e709880aff725e5a0140bfdb43475b33d454bb
proXDhiya <prox.dhiya@gmail.com>
2021-08-29 20:47:43 +0100

    complete app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const share = document.querySelector('.share');
+const shareBox = document.querySelector('.share-box');
+const shareBoxTr = document.querySelector('.triangle');
+
+let state = 0;
+
+share.addEventListener(
+    'click', () => {
+        if(state == 0) {
+            state = 1;
+            shareBox.classList.remove('hidden');
+            shareBoxTr.classList.remove('hidden');
+        }
+        else {
+            state = 0;
+            shareBox.classList.add('hidden');
+            shareBoxTr.classList.add('hidden');
+        }
+    }
+)
